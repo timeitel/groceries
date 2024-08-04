c5848d17a8fba37cef9577a01cba95a2c35b5430
Tim Eitel <timothy.eitel@gmail.com>
2024-08-04 23:18:36 +1000

    feat: remove secret, only need env for local

--- internal/services/db/db.go
+++ internal/services/db/db.go
@@ -1,56 +1,20 @@
 package db
 
 import (
-	"bufio"
 	"database/sql"
+	"log"
 	"os"
 
 	_ "github.com/tursodatabase/go-libsql"
 )
 
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func Init() *sql.DB {
-	path := os.Getenv("DB_URL_FILE")
-
-	file, err := os.Open(path)
-	check(err)
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	url := scanner.Text()
+	url := os.Getenv("DB_URL")
 
 	db, err := sql.Open("libsql", url)
-	check(err)
-
-	// TODO: seed db / move to build / cache layer
-	const create string = `
-  CREATE TABLE IF NOT EXISTS items (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR NOT NULL UNIQUE,
-  description TEXT
-  );`
-
-	_, err = db.Exec(create)
-	check(err)
-
-	const insertApple string = `
-  INSERT OR IGNORE INTO items (name, description)
-  VALUES ('Apple', 'A delicious fruit.');
-  `
-	_, err = db.Exec(insertApple)
-	check(err)
-
-	const insertBanana string = `
-  INSERT OR IGNORE INTO items (name, description)
-  VALUES ('Banana', 'Another delicious fruit.');
-  `
-	_, err = db.Exec(insertBanana)
-	check(err)
+	if err != nil {
+		log.Fatal("Unable to open db", url, err)
+	}
 
 	return db
 }
