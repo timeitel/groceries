c5f06f43ae7058e76f7c389e0d15f9f5addf34ef
Tim Eitel <timothy.eitel@gmail.com>
2024-08-06 18:13:58 +1000

    feat: seeding and schema

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -4,3 +4,16 @@ CREATE TABLE IF NOT EXISTS items (
   description TEXT
 );
 
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shopping_lists (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  item_id INTEGER,
+  user_id INTEGER,
+  FOREIGN KEY (item_id) REFERENCES items(item_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
--- scripts/seed.sql
+++ scripts/seed.sql
@@ -4,3 +4,9 @@ VALUES ('Apple', 'A delicious fruit.');
 INSERT OR IGNORE INTO items (name, description)
 VALUES ('Banana', 'Another delicious fruit.');
 
+INSERT OR IGNORE INTO users (id, name)
+VALUES (1, 'Cool guy');
+
+INSERT OR IGNORE INTO shopping_lists (item_id, user_id)
+VALUES (1, 1);
+
