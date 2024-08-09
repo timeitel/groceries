8c9788859e2edd11a6d83a2cb638533a6e66291f
Tim Eitel <timothy.eitel@gmail.com>
2024-08-09 17:53:14 +1000

    wip: getting admin user

--- internal/domain/shopper/libsql/user.go
+++ internal/domain/shopper/libsql/user.go
@@ -1,13 +1,32 @@
 package libsql
 
-import "github.com/timeitel/groceries/internal/common/models"
+import (
+	"fmt"
+	"log"
+
+	"github.com/timeitel/groceries/internal/common/models"
+)
 
 func (r *Repository) GetUser() (models.User, error) {
-	r.DB.QueryRow("")
+	query := "SELECT id, name, is_admin FROM users LIMIT 1"
+
+	row := r.DB.QueryRow(query)
+
+	var id string
+	var name string
+	var isAdmin bool
+
+	err := row.Scan(&id, &name, &isAdmin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("USER: %s, %s %b \n", id, name, isAdmin)
 
 	user := models.User{
-		Id:   "asdfasd",
-		Name: "cool guy",
+		Id:      id,
+		Name:    name,
+		IsAdmin: isAdmin,
 	}
 
 	return user, nil
--- scripts/seed.sql
+++ scripts/seed.sql
@@ -4,9 +4,9 @@ VALUES ('Apple', 'A delicious fruit.');
 INSERT OR IGNORE INTO items (name, description)
 VALUES ('Banana', 'Another delicious fruit.');
 
-INSERT OR IGNORE INTO users (id, name)
-VALUES (1, 'Cool guy');
+INSERT OR IGNORE INTO users (name, is_admin)
+VALUES ('Cool guy', 1);
 
-INSERT OR IGNORE INTO shopping_lists (item_id, user_id)
+INSERT OR IGNORE INTO carts (item_id, user_id)
 VALUES (1, 1);
 
