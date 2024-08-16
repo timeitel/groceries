f9af11599b949573ed1ad693715b7db2fba65baa
Tim Eitel <timothy.eitel@gmail.com>
2024-08-16 18:13:24 +1000

    feat: js static

---
+++ internal/web/static/js/events.js
@@ -0,0 +1,6 @@
+document.body.addEventListener("htmx:beforeSwap", function (evt) {
+  if (evt.detail.xhr.status === 422) {
+    evt.detail.shouldSwap = true;
+    evt.detail.isError = false;
+  }
+});
