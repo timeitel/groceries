9e70a1b13ead8bcc7d3911f04d198f96a223b7fa
Tim Eitel <timothy.eitel@gmail.com>
2024-08-19 18:50:38 +1000

    chore: update tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./internal/web/**/*.{html,templ}"],
+  content: ["./internal/web/views/**/*.templ"],
   theme: {
     extend: {},
   },
