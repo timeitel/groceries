dd94250ea4d18f21247d75b5e39aa3cb12cf57f8
Tim Eitel <timothy.eitel@gmail.com>
2024-09-15 11:27:09 +1000

    feat: seeding initial item

--- cmd/db/main.go
+++ cmd/db/main.go
@@ -40,6 +40,28 @@ func main() {
 		log.Fatalln("Creating cart", err)
 	}
 
+	item, err := createItem(conn, ctx, "Apple", "Delicious")
+	if err != nil {
+		log.Fatalln("Creating item", err)
+	}
+
 	fmt.Printf("User created: %s, admin: %v\n", user.Name, user.IsAdmin.Bool)
 	fmt.Printf("Cart created: %v\n", cart.Name.String)
+	fmt.Printf("Initial item created: %v\n", item)
 }
+
+type item = db.Item
+
+func createItem(db *sql.DB, ctx context.Context, name, description string) (db.Item, error) {
+	row := db.QueryRowContext(ctx, createItemQuery, name, description)
+	var i item
+	err := row.Scan(&i.ID, &i.Name, &i.Description)
+	return i, err
+}
+
+const createItemQuery = `-- name: CreateItem :one
+INSERT INTO items (id, name, description)
+    VALUES (1000, ?, ?)
+RETURNING
+    id, name, description
+`
